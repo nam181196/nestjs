c948f9a9522119f630645f6f6cd409e262154318
nam181196 <102805613+nam181196@users.noreply.github.com>
2024-07-25 10:30:21 +0700

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { ProductsModule } from './products/products.module';
       synchronize: true,
     }),
     ProductsModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
