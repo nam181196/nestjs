3d6f1184e869c6efbdcbd1c50103a03ec985613e
nam181196 <102805613+nam181196@users.noreply.github.com>
2024-07-25 10:01:47 +0700

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    ProductsModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ products/dto/create.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsNumber()
+  stock: number;
+}
